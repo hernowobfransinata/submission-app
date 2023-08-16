784d53e98fa6c8d0eebc373d7a3290fef1e81168
hernowobfransinata <fransinata@gmail.com>
2023-08-16 13:10:35 +0700

    session, submission

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://cdn.tailwindcss.com"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <body class="bg-gray-200 font-sans">
 
@@ -13,7 +14,8 @@
     @yield('content')
 
     <x-footer></x-footer>
-
+    
+    <x-flash></x-flash>
 </body>
 
 </html>
